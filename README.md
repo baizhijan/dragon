8d0a9fbf9d360e760e8632dcfb22af48f4fddf0e
aaronbai <aaronbai@tcl.com>
2016-10-09 16:38:23 +0800

    添加数据源

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+项目结构:
+    spring+springMVC+logback
+
+一 项目初始化,提交到github
+二 添加数据源
+三 service层单纯java项目执行
+
+
+
 1.支付功能
 2.库存功能
 3.流水对账
---
+++ dragon-dao/src/main/java/com/bzj/daagon/dao/Test.java
@@ -0,0 +1,9 @@
+package com.bzj.daagon.dao;
+
+/**
+ * User:aaronbai@tcl.com
+ * Date:2016-10-09
+ * Time:15:07
+ */
+public interface Test {
+}
